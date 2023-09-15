f21c383935db2bf37a4f0d65c687932e28711989
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-09-15 17:38:47 +0300

    feat: complite first and second lessons of ToDoList homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { TaskType, TodoList } from './TodoList';
+
+export type FilterValuesType = "all" | "completed" | "active"
 
 function App() {
+
+    let [tasks, setTasks] = useState<Array<TaskType>>([
+        {id: 1, title:"HTML&CSS", isDone: true},
+        {id: 2, title:"JS", isDone: true},
+        {id: 3, title:"React", isDone: false}
+    ]);
+
+
+    function remomeTask(id:number) {
+
+        let filteredTasks = tasks.filter( i => i.id !== id)
+        setTasks(filteredTasks);
+    }
+
+    function changeFilter (value: FilterValuesType){
+        setFilter(value);
+    }
+
+    let [filter, setFilter] = useState<FilterValuesType>("all");
+
+    let taskForTodolist = tasks;
+    if (filter === "completed") {
+        taskForTodolist = tasks.filter(i => i.isDone === true);
+    }
+    if (filter === "active") {
+        taskForTodolist = tasks.filter(i => i.isDone === false);
+    }
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList   title="What to Learn" 
+                        tasks = {taskForTodolist}
+                        removeTask = {remomeTask}
+                        changeFilter = {changeFilter}
+            />
         </div>
     );
+
 }
+
 
 export default App;
