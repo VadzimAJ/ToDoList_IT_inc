88d5453e815dcf34b00b737f51aeafee57d5b259
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-09-20 00:53:22 +0300

    merge: macke merge

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 type ButtonType = {
   title: string;
-<<<<<<< HEAD
   callBack: (title?: string | undefined) => void;
-=======
-  callBack: (title?: string) => void;
->>>>>>> e502f39e63137ad4564cc17930241ac30446ef14
-};
 
 export const Button = (props: ButtonType) => {
   const onClickHandler = () => {
