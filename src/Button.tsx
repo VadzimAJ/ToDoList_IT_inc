e502f39e63137ad4564cc17930241ac30446ef14
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-09-19 17:16:12 +0300

    feat: complite button_lesson task

---
+++ src/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type ButtonType = {
+  title: string;
+  callBack: (title?: string) => void;
+};
+
+export const Button = (props: ButtonType) => {
+  const onClickHandler = () => {
+    props.callBack(props.title); // Передаем title в callBack
+  }
+
+  return (
+    <button onClick={onClickHandler}>{props.title}</button>
+  )
+}
