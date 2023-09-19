1f71263db7970b32cbb9d871c254c12f4f972ca7
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-09-19 17:16:12 +0300

    feat: complite button_lesson task

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Button } from './Button';
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+  const Button1Foo = (subScr: string, age?: number, title?: string) => {
+    console.log('Hello Im ' + subScr, 'Age: ' + age, 'it`s ' + title + ' Button.');
+  }
+
+  const Button2Foo = (subScr: string, age?: number, title?: string) => {
+    console.log('Hello Im ' + subScr, 'Age: ' + age, 'it`s ' + title + ' Button.');
+  }
+
+  const Button3Foo = ( title?: string) => {
+    console.log( title + ' Button.');
+  }
+
+  return (
+    <div className="App">
+      <Button title={'First'} callBack={(title) => Button1Foo('Rolly', 21, title)} />
+      <Button title={'Second'} callBack={(title) => Button2Foo('Jager', 23, title)} />
+      <Button title={'Stuped'} callBack={(title) => Button3Foo(title)} />
+    </div>
+  );
 }
 
 export default App;
