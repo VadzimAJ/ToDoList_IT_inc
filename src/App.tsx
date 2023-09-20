0f8555897576d617ec6ef560ccaa34865870350b
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-09-20 06:10:25 +0300

    feat: fiand a strange exeption on FilteredType

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { ReactDOM } from 'react';
 import './index.css'
 import './App.css';
-import { StudentComponent } from './StudentComponent';
+import { MoneyComponent } from './StudentComponent';
 
 
 
@@ -13,33 +13,19 @@ import { StudentComponent } from './StudentComponent';
 
 function App() {
 
-    let [students, setStudents] = useState([
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100}
-    ]);
+  const [money, setMoney] = useState([
+    { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+    { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+    { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+  ])  
 
        return (
-        <StudentComponent students={students}/>
+        <MoneyComponent money={money}/>
     );
 }
 
